1a1a6770075f011afc0a8d4b3e2e211da5bdfc01
dk6200 <dk6200614657.m23@gmail.com>
2024-07-04 17:35:56 +0530

    Revert "complete project"
    This reverts commit 103ec511e2be91e9bfcdae9d094193ea418c3724.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import Herosection from "./Components/Herosection";
-import Habot from "./Components/Habot";
-import SupplierInfo from "./Components/SupplierInfo";
-import HowItWorks from "./Components/HowItWorks";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import "./index.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="">
-      <Header />
-      <Herosection />
-      <Habot />
-      <SupplierInfo />
-      <HowItWorks />
-      <Footer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
